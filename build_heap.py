156330a681fecdad0f53bedb74f136ac35ebc8e0
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 21:56:27 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,8 +1,8 @@
 # python3
-def heapify_down(arr, n, i, swaps):
-    """
-    Perform a single swap operation and ensure that the min-heap property is maintained.
-    """
+swaps = []
+
+def heapify_down(arr, n, i):
+    global swaps
     min_idx = i
     l = 2 * i + 1
     r = 2 * i + 2
@@ -13,30 +13,36 @@ def heapify_down(arr, n, i, swaps):
     if i != min_idx:
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
         swaps.append((i, min_idx))
-        heapify_down(arr, n, min_idx, swaps)
+        heapify_down(arr, n, min_idx)
 
 
 def build_heap(arr):
     n = len(arr)
-    swaps = []
     # Start from the last non-leaf node and perform heapify_down on each node
     for i in range(n // 2, -1, -1):
-        heapify_down(arr, n, i, swaps)
+        heapify_down(arr, n, i)
     return swaps
+
+
 def main():
-    
-    input_type = input("Enter input type (I for keyboard input, F for file input): ")
+    input_type = input("I or F: ")
 
     if input_type == "I":
-    # Read input from keyboard
+        # Read input from keyboard
         n = int(input().strip())
         arr = list(map(int, input().split()))
     else:
-    # Read input from file
-        filename = input("Enter filename: ").strip()
-        with open(filename, "r") as f:
-            n = int(f.readline().strip())
-            arr = list(map(int, f.readline().split()))
+        # Read input from file
+        while True:
+            filename = input("Enter filename: ").strip()
+            try:
+                with open(filename, "r") as f:
+                    n = int(f.readline().strip())
+                    arr = list(map(int, f.readline().split()))
+                    break
+            except FileNotFoundError:
+                print("File not found. Please try again.")
+
     swaps = build_heap(arr)
     print(len(swaps))
     for i, j in swaps:
