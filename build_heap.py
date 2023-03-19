2366a0103fbe217905707ad5335f72eceb2c1844
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 22:03:52 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,53 +1,38 @@
-# python3
-swaps = []
-
-def heapify_down(arr, n, i):
-    global swaps
-    min_idx = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-    if l < n and arr[l] < arr[min_idx]:
-        min_idx = l
-    if r < n and arr[r] < arr[min_idx]:
-        min_idx = r
-    if i != min_idx:
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        swaps.append((i, min_idx))
-        heapify_down(arr, n, min_idx)
-
+def sift_down(arr, i, swaps):
+    n = len(arr)
+    min_index = i
+    left_child = 2*i + 1
+    if left_child < n and arr[left_child] < arr[min_index]:
+        min_index = left_child
+    right_child = 2*i + 2
+    if right_child < n and arr[right_child] < arr[min_index]:
+        min_index = right_child
+    if i != min_index:
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        swaps.append((i, min_index))
+        sift_down(arr, min_index, swaps)
 
 def build_heap(arr):
     n = len(arr)
-    # Start from the last non-leaf node and perform heapify_down on each node
-    for i in range(n // 2, -1, -1):
-        heapify_down(arr, n, i)
+    swaps = []
+    for i in range(n//2, -1, -1):
+        sift_down(arr, i, swaps)
     return swaps
 
-
 def main():
-    input_type = input("I or F: ")
-
-    if input_type == "I":
-        # Read input from keyboard
-        n = int(input().strip())
-        arr = list(map(int, input().split()))
-    else:
-        # Read input from file
-        while True:
-            filename = input("Enter filename: ").strip()
-            try:
-                with open(filename, "r") as f:
-                    n = int(f.readline().strip())
-                    arr = list(map(int, f.readline().split()))
-                    break
-            except FileNotFoundError:
-                print("File not found. Please try again.")
-
+    text = input()
+    if 'I' in text:
+        text = input()
+    elif 'F' in text:
+        file = "tests/04"
+        with open(file) as f:
+            text = f.read()
+    arr = list(map(int, text.split()))
     swaps = build_heap(arr)
+
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
