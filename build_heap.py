00abedbced5052c3436ea7dc7107ddb16665aeb4
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 22:16:25 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,51 +1,45 @@
-#KristersRicardsAunins
-import pathlib
-
-def main():
-    input_type = input()
-    parser = {
-        'I': parse_input,
-        'F': parse_file
-    }
-    n, data = parser[input_type]()
-    swaps = build_heap(data)
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+import os
+#KristersRicardsAunins221RDC033
 def build_heap(data):
     swaps = []
-    heap_size = len(data)
-    for i in range(heap_size // 2, -1, -1):
+    lielums = len(data)
+    for i in range(lielums // 2, -1, -1):
         sift_down(data, i, swaps)
     return swaps
 
 def sift_down(data, i, swaps):
     heap_size = len(data)
-    min_index = i
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2 
-    if left_child < heap_size and data[left_child] < data[min_index]:
-        min_index = left_child
-    if right_child < heap_size and data[right_child] < data[min_index]:
-        min_index = right_child
-    if min_index != i:
+    while True:
+        min_index = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+        if left_child < heap_size and data[left_child] < data[min_index]:
+            min_index = left_child
+        if right_child < heap_size and data[right_child] < data[min_index]:
+            min_index = right_child
+        if min_index == i:
+            break
         swaps.append((i, min_index))
         data[i], data[min_index] = data[min_index], data[i]
-        sift_down(data, min_index, swaps)
+        i = min_index
 
-def parse_input():
-    n = int(input())
-    data = list(map(int, input().split()))
-    return n, data
-
-def parse_file():
-    filename = input().strip()
-    file_path = pathlib.Path('tests') / filename
-    with file_path.open(mode="r") as file:
-        n = int(file.readline().strip())
-        data = list(map(int, file.readline().split()))
-    return n, data
-
+def main():
+    ievade = input()
+    if "I" in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif "F" in ievade:
+        fails = input()
+        atrasanas = './tests/'
+        faila_vieta = os.path.join(atrasanas, fails)
+        with open(faila_vieta, mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
+    swaps = build_heap(data)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+        
 if __name__ == "__main__":
     main()
