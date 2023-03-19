ad2b3dfcaa1df2fab8f2a365cb43c03df2074855
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 22:11:41 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,44 @@
-def sift_down(arr, i, swaps):
-    n = len(arr)
-    min_index = i
-    left_child = 2*i + 1
-    if left_child < n and arr[left_child] < arr[min_index]:
-        min_index = left_child
-    right_child = 2*i + 2
-    if right_child < n and arr[right_child] < arr[min_index]:
-        min_index = right_child
-    if i != min_index:
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-        swaps.append((i, min_index))
-        sift_down(arr, min_index, swaps)
+#KristersRicardsAunins
+import os
 
-def build_heap(arr):
-    n = len(arr)
+def build_heap(data):
     swaps = []
-    for i in range(n//2, -1, -1):
-        sift_down(arr, i, swaps)
+    lielums = len(data)
+    for i in range(lielums // 2, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
 
-def main():
-    text = input()
-    if 'I' in text:
-        text = input()
-    elif 'F' in text:
-        file = "tests/04"
-        with open(file) as f:
-            text = f.read()
-    arr = list(map(int, text.split()))
-    swaps = build_heap(arr)
+def sift_down(data, i, swaps):
+    lielums = len(data)
+    min_index = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2 
+    if left_child < lielums and data[left_child] < data[min_index]:
+        min_index = left_child
+    if right_child < lielums and data[right_child] < data[min_index]:
+        min_index = right_child
+    if min_index != i:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(data, min_index, swaps)
 
+def main():       
+    ievade = input()
+    if "I" in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif "F" in ievade:
+        fails = input()
+        atrasanas = './tests/'    
+        faila_vieta = os.path.join(atrasanas, fails)           
+        with open(faila_vieta, mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
+    swaps = build_heap(data)
     print(len(swaps))
     for i, j in swaps:
-        print(i, j)
+        print(i, j)        
 
 if __name__ == "__main__":
     main()
