0f04433d83dd600793babab6c52b80bb0e2ce01c
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 21:47:09 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,59 +1,33 @@
 # python3
-def parent(i):
+def heapify_down(arr, n, i, swaps):
+    """
+    Perform a single swap operation and ensure that the min-heap property is maintained.
+    """
+    min_idx = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and arr[l] < arr[min_idx]:
+        min_idx = l
+    if r < n and arr[r] < arr[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        swaps.append((i, min_idx))
+        heapify_down(arr, n, min_idx, swaps)
 
-    return (i-1)//2
 
-def left_child(i):
-
-    return 2*i + 1
-
-def right_child(i):
-
-    return 2*i + 2
-
-def sift_down(data, swaps, i):
-
-# Find the maximum element among i and its children
-    max_index = i
-    l = left_child(i)
-    if l < len(data) and data[l] < data[max_index]:
-        max_index = l
-        r = right_child(i)
-    if r < len(data) and data[r] < data[max_index]:
-        max_index = r
-# If i is not the maximum, swap it with the maximum and continue sifting down
-    if i != max_index:
-        swaps.append((i, max_index))
-        data[i], data[max_index] = data[max_index], data[i]
-    sift_down(data, swaps, max_index)
-
-def build_heap(data):
-
+def build_heap(arr):
+    n = len(arr)
     swaps = []
-# Start from the last node and work upwards to the root node
-    for i in range(len(data)//2, -1, -1):
-        sift_down(data, swaps, i)
-    return swap
-
-
+    # Start from the last non-leaf node and perform heapify_down on each node
+    for i in range(n // 2, -1, -1):
+        heapify_down(arr, n, i, swaps)
+    return swaps
 def main():
     
-    input_type = input()
-    if input_type == "I":
-        n = int(input())
-        data = list(map(int, input().split()))
-
-    elif input_type == "F":
-        with open(" ", "r") as f:
-            n = int(f.readline())
-            data = list(map(int, f.readline().split()))
-
-    else:
-        print("Invalid")
-        return
-
-    assert len(data) == n
-    swaps = build_heap(data)
+    n = int(input())
+    arr = list(map(int, input().split()))
+    swaps = build_heap(arr)
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
