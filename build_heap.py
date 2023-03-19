5876376914e057043a17b8b30b9233a6427c5d56
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-03-19 21:37:56 +0200

    test

--- build_heap.py
+++ build_heap.py
@@ -1,26 +1,18 @@
 # python3
 def parent(i):
-"""
-This function returns the index of the parent of node i
-"""
+
 return (i-1)//2
 
 def left_child(i):
-"""
-This function returns the index of the left child of node i
-"""
+
 return 2*i + 1
 
 def right_child(i):
-"""
-This function returns the index of the right child of node i
-"""
+
 return 2*i + 2
 
 def sift_down(data, swaps, i):
-"""
-This function sifts down the element at index i to maintain the heap property
-"""
+
 # Find the maximum element among i and its children
 max_index = i
 l = left_child(i)
@@ -36,9 +28,7 @@ data[i], data[max_index] = data[max_index], data[i]
 sift_down(data, swaps, max_index)
 
 def build_heap(data):
-"""
-This function converts the input array into a heap using O(n) swaps
-"""
+
 swaps = []
 # Start from the last node and work upwards to the root node
 for i in range(len(data)//2, -1, -1):
@@ -48,8 +38,8 @@ return swap
 
 def main():
     
-    input_type = input("Enter input type (K for keyboard input, F for file input): ")
-    if input_type == "K":
+    input_type = input()
+    if input_type == "I":
         n = int(input())
         data = list(map(int, input().split()))
 
@@ -59,7 +49,7 @@ def main():
             data = list(map(int, f.readline().split()))
 
     else:
-        print("Invalid input type. Enter K for keyboard input or F for file input.")
+        print("Invalid")
         return
 
     assert len(data) == n
